3b3ac6ea9461d08e4976e420063ff63801ce8495
Filipe Melo <filipemelo032@gmail.com>
2023-10-19 23:14:51 -0300

    feature: improve test exampleÃ

--- src/test/test.pm
+++ src/test/test.pm
@@ -1,8 +1,63 @@
-int a = 2;
-int b = 5;
 
-func sum(int x, int y) {
-    x = x + y;
+int a = 0;
+int b = 0;
+boolean isBigger = a > b;
+boolean areEqual = a == b;
+boolean isValid = true;
+boolean notValid = false;
+float height = 1.80;
+str name = "ProgMatic";
+char character = 'a';
+int-> aPointer = $a;
+int-> bPointer = $b;
+
+
+
+int sum = 34 + 25;
+int sub = 2023 - 2002;
+int multiply = 34 * 75;
+int division = 234340 / 342;
+int mod = 4 % 2;
+int equation = 45 * 545 - 45 / (4 -2);
+boolean orResult = sum || multiply;
+boolean andResult = mod && multiply;
+boolean notResult = !equation;
+boolean acceptRequest = false;
+boolean refuseRequest = true;
+
+
+
+int a =0;
+
+loop(a < 1000) {
+  print("Counting A: ");
+  print(a);
+  a = a + 1;
 }
 
-sum(a, b);
+
+str name = "";
+int birthYear;
+print("what is your name?");
+read(name);
+print("what year were you born?");
+read(birthYear);
+print("Your age is: ");
+print(2023  - birthYear);
+
+
+int age = 18;
+if (age < 18) {
+ print("You are underage!");
+} else {
+ print("You are not underage!");
+}
+
+
+int a = 10;
+int b = 20;
+func sumNumber(int numberA, int numberB) {
+  numberA = numberA + numberB;
+}
+sumNumber(a, b);
+print(a);
