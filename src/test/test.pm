47d4007a8422d868cf79da55529cd1b016df1343
Filipe Melo <filipemelo032@gmail.com>
2023-11-26 21:10:45 -0300

    feature: update example code with running program

--- src/test/test.pm
+++ src/test/test.pm
@@ -1,63 +1,23 @@
-int a = 0;
-    x = x + y;	int b = 0;
-boolean isBigger = a > b;
-boolean areEqual = a == b;
-boolean isValid = true;
-boolean notValid = false;
-float height = 1.80;
-str name = "ProgMatic";
-char character = 'a';
-int-> aPointer = $a;
-int-> bPointer = $b;
+func calcYearOfBirth() {
 
+    int age;
+    int-> agePointer = $age;
+    int year;
+    int-> yearPointer;
 
+    int yearOfBirth;
+  print("Qual o ano atual?");
+  yearPointer = $year;
 
-int sum = 34 + 25;
-int sub = 2023 - 2002;
-int multiply = 34 * 75;
-int division = 234340 / 342;
-int mod = 4 % 2;
-int equation = 45 * 545 - 45 / (4 -2);
-boolean orResult = sum || multiply;
-boolean andResult = mod && multiply;
-boolean notResult = !equation;
-boolean acceptRequest = false;
-boolean refuseRequest = true;
+  read(yearPointer);
 
+   print("Qual ano você nasceu? ");
+   read(yearOfBirth);
 
+    &agePointer = year - yearOfBirth;
 
-int a =0;
+    print("Sua idade: ");
+    print(age);
 
-loop(a < 1000) {
-  print("Counting A: ");
-  print(a);
-  a = a + 1;
 }
-
-
-sum(a, b);
-str name = "";
-int birthYear;
-print("what is your name?");
-read(name);
-print("what year were you born?");
-read(birthYear);
-print("Your age is: ");
-print(2023  - birthYear);
-
-
-int age = 18;
-if (age < 18) {
- print("You are underage!");
-} else {
- print("You are not underage!");
-}
-
-
-int a = 10;
-int b = 20;
-func sumNumber(int numberA, int numberB) {
-  numberA = numberA + numberB;
-}
-sumNumber(a, b);
-print(a);
+calcYearOfBirth();
